fc0d3977bf59ef288bddc730e7dacaca2363b388
Neal Shinoda <35791603+JLU-Neal@users.noreply.github.com>
2021-04-15 20:35:37 +0800

    Find the ground
    By projecting the point cloud data to axis y, the frequency of the points is saved.
    Find the height of the ground for cutting irrelevant information.

--- Transform.py
+++ Transform.py
@@ -20,6 +20,7 @@ class Transform():
         self.fy = 388.21179 / self.scale
         self.rows = 480 // self.scale
         self.cols = 640 // self.scale
+        self.threshold = None
     def exec(self, depth_bg):
         before = time.time()
         depth_bg = cv2.resize(depth_bg, (0, 0), fx=1./self.scale, fy=1./self.scale, interpolation=cv2.INTER_NEAREST)
@@ -59,16 +60,31 @@ class Transform():
         # xyzs = np.matmul(xyzs, rotationY)
         xyzs = np.matmul(xyzs, rotationZ)
 
+
+        # find the ground
+        if(self.threshold is None):
+            point_count_height = np.zeros(1000)
+            for index in range(xyzs.shape[0]):
+                height = 1500 - xyzs[index, 1]
+                point_count_height[int(height)] = point_count_height[int(height)] + 1
+
+            x = 1500 - np.arange(point_count_height.shape[0])
+            y = point_count_height
+            # plt.plot(x, y)
+            # plt.show()
+            ground_mask = y > 100
+            self.threshold = x[ground_mask].min() - 30
+
         # crop
-        mask = (xyzs[:, 0] > -1200) * (xyzs[:, 0] < 1200) * (xyzs[:, 1] < 1200) * (xyzs[:, 2] > -1500) * (xyzs[:, 2 ] < 1200)
+        mask = (xyzs[:, 0] > -1200) * (xyzs[:, 0] < 1200) * (xyzs[:, 1] < self.threshold) * (xyzs[:, 2] > -1500) * (xyzs[:, 2 ] < 1200)
         xyzs = xyzs[mask]
 
         after = time.time()
         print("tran time: "+str(after - before))
-        # fig = plt.figure()
-        # ax = mplot3d.Axes3D(fig)
-        # ax.scatter3D(xyzs.T[0], xyzs.T[1], xyzs.T[2])  # 散点图
-        # plt.show()
+        fig = plt.figure()
+        ax = mplot3d.Axes3D(fig)
+        ax.scatter3D(xyzs.T[0], xyzs.T[1], xyzs.T[2])  # 散点图
+        plt.show()
 
         return xyzs
 
